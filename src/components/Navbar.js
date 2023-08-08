a6068ae1fd8009b0e7d2f132b951516a7e03c1ca
Amine chabane <chabane.amine@ofppt-edu.ma>
2023-08-08 21:45:15 +0100

    edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,7 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
+          <img src={Layer 1} className="img-fluid logo" alt="brand" />
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
